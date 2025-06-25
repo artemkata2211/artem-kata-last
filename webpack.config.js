b3967199f9175e654d270d74f18fad31c7eb9b1a
Artemka2211 <poxitivik@yandex.ru>
2025-06-25 11:03:31 +0500

    last

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   // Входной файл
   entry: [
     './src/js/index.js'
@@ -14,8 +15,13 @@ module.exports = {
     filename: './js/bundle.js'
   },
 
+  externals: {
+    'swiper': 'Swiper'
+  },
+
   // Source maps для удобства отладки
   devtool: "source-map",
+
 
   module: {
     rules: [
@@ -48,7 +54,10 @@ module.exports = {
         test: /\.(eot|ttf|woff|woff2)$/,
         use: [
           {
-            loader: 'file-loader?name=./fonts/[name].[ext]'
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+            },
           },
         ]
       },
